a85b2239dadbf27452ea884f7f5fb056dec00db1
sudipto <sudiptocsi@gmail.com>
2021-09-04 21:06:21 +0600

    first code

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+class Navbar extends React.Component {
+    render() { 
+        return(
+            <nav>
+                <h1>Context App</h1>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Country</li>
+                </ul>
+            </nav>
+        );
+    }
+}
+export default Navbar;
