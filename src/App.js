6bb39715005b6593dd49183cd31b1d40458d48db
sudipto <sudiptocsi@gmail.com>
2021-09-06 21:05:02 +0600

    updatd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import BookLists from "./components/BookLists";
 import Navbar from "./components/Navbar";
+import ThemeContextProvider from "./contexts/ThemeContext";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <BookLists/>
+      <ThemeContextProvider>
+        <Navbar/>
+        <BookLists/>
+      </ThemeContextProvider>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 class Navbar extends React.Component {
+    //static contextType = ThemeContext;
     render() { 
-        return(
-            <nav>
-                <h1>Context App</h1>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Country</li>
-                </ul>
-            </nav>
+        return(    
+            <ThemeContext.Consumer>{(context) => { 
+                const { isLightTheme, light, dark } = context;
+                const theme = isLightTheme ? light : dark;
+                <nav style={{ background: theme.ui, color: theme.syntax }}>
+                    <h1>Context App</h1>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Country</li>
+                    </ul>
+                </nav>
+            }}
+            </ThemeContext.Consumer>
         );
     }
 }
