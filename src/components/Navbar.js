3db3f00c576ca4dc56acea92563a2f0ce2c5169f
sudipto <sudiptocsi@gmail.com>
2021-09-16 21:02:33 +0600

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,56 @@
-import React, { Component } from 'react';
-import { ThemeContext } from '../contexts/ThemeContext';
+import React, { Component, Fragment } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
+
+// function test(params) {
+//   console.log(params);
+// }
+
+// function Navbar() {
+//   test();
+//   return (
+//     <Fragment>
+//       Hello
+//     </Fragment>
+//   );
+// }
+
+//export default Navbar;
+
 
 class Navbar extends Component {
-  // static contextType = ThemeContext;
+  static contextType = ThemeContext;
+  constructor(){
+    super();
+    console.log("constructor called here");
+  }
+  static contextType = ThemeContext;
+  test = () => {
+    console.log("Printing logger method......");
+  };
   render() {
-    return ( 
-      <ThemeContext.Consumer>{(context) => {
-        const { isLightTheme, light, dark } = context;
-        const theme = isLightTheme ? light : dark;
-        return (
-          <nav style={{ background: theme.ui, color: theme.syntax }}>
-            <h1>Context App</h1>
-            <ul>
-              <li>Home</li>
-              <li>About</li>
-              <li>Contact</li>
-            </ul>
-          </nav>
-        )
-      }}</ThemeContext.Consumer>
+    //this.test(10, 'sudipto karmoker.....');
+    //const { isLightTheme, light, dark } = this.context;
+    // const isLightTheme = this.context.isLightTheme;
+    // const light = this.context.light;
+    // const dark = this.context.dark;
+
+    // console.log(isLightTheme) ;
+    // console.log(light);
+    // console.log(dark);
+
+    return (
+      <Fragment>
+        <nav>
+          <h1>Context App</h1>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+      </Fragment>
     );
   }
 }
- 
+
 export default Navbar;
